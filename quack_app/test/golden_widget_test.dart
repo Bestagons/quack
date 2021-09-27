28af8b3f8f77094498d50eed718e37adfdba22f7
zjdavid <zjdavid.2003@gmail.com>
2021-09-27 14:36:02 -0400

    Add Groups to test

--- quack_app/test/golden_widget_test.dart
+++ quack_app/test/golden_widget_test.dart
@@ -5,12 +5,14 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   var test1Name = 'Homepage';
-  testGoldens(test1Name, (WidgetTester tester) async {
-    tester.binding.window.physicalSizeTestValue = const Size(1170, 2532);
-    tester.binding.window.devicePixelRatioTestValue = 3.0;
-    await loadAppFonts();
-    await tester.pumpWidget(const MyApp());
-    await expectLater(find.byType(MyApp),
-        matchesGoldenFile('goldens/$test1Name.png'));
+  group('HomepageTests', () {
+    testGoldens(test1Name, (WidgetTester tester) async {
+      tester.binding.window.physicalSizeTestValue = const Size(1170, 2532);
+      tester.binding.window.devicePixelRatioTestValue = 3.0;
+      await loadAppFonts();
+      await tester.pumpWidget(const MyApp());
+      await expectLater(find.byType(MyApp),
+          matchesGoldenFile('goldens/$test1Name.png'));
+    });
   });
 }
