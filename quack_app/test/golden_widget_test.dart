c4a178e844873be039f6b4a2a44c7042ae244bae
zjdavid <zjdavid.2003@gmail.com>
2021-09-27 14:59:30 -0400

    Rename test

--- quack_app/test/golden_widget_test.dart
+++ quack_app/test/golden_widget_test.dart
@@ -4,15 +4,15 @@ import 'package:quack_app/main.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
-  var test1Name = 'homepage_test';
-  group(test1Name, () {
-    testGoldens(test1Name, (WidgetTester tester) async {
+  var homepageUITest = 'homepage_ui_test';
+  group(homepageUITest, () {
+    testGoldens(homepageUITest, (WidgetTester tester) async {
       tester.binding.window.physicalSizeTestValue = const Size(1170, 2532);
       tester.binding.window.devicePixelRatioTestValue = 3.0;
       await loadAppFonts();
       await tester.pumpWidget(const MyApp());
       await expectLater(find.byType(MyApp),
-          matchesGoldenFile('goldens/$test1Name.png'));
+          matchesGoldenFile('goldens/$homepageUITest.png'));
     });
   });
 }
