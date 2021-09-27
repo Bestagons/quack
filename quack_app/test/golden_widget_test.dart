bd18146a71a67b46d8d81689d5a458e6dfe094e2
zjdavid <zjdavid.2003@gmail.com>
2021-09-27 10:15:25 -0400

    Add constraint for screen resolution

--- quack_app/test/golden_widget_test.dart
+++ quack_app/test/golden_widget_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/widgets.dart';
 import 'package:quack_app/main.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
+
   testWidgets('Golden test', (WidgetTester tester) async {
+    tester.binding.window.physicalSizeTestValue = const Size(1170, 2532);
+    tester.binding.window.devicePixelRatioTestValue = 3.0;
     await loadAppFonts();
     await tester.pumpWidget(const MyApp());
     await expectLater(find.byType(MyApp),
