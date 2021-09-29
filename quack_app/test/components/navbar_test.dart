9742b23f20dbd2e58c27713559c9afc976dccc51
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-09-29 18:29:54 -0400

    Add golden tests for navbar UI

---
+++ quack_app/test/components/navbar_test.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quack_app/components/navbar.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:quack_app/constants/constants.dart';
+
+void main() {
+  group("NavBar Test", () {
+    const MaterialApp app = MaterialApp(home: NavBar());
+    testGoldens("settings_active", (WidgetTester tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(app);
+      await tester.tap(find.byIcon(Icons.settings_outlined));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await expectLater(find.byType(NavBar),
+          matchesGoldenFile('goldens/settings_active.png'));
+    });
+
+    testGoldens("notifications_active", (WidgetTester tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(app);
+      await tester.tap(find.byIcon(Constants.kNavBarNotificationsIcon));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await expectLater(find.byType(NavBar),
+          matchesGoldenFile('goldens/notifications_active.png'));
+    });
+
+    testGoldens("dining_active", (WidgetTester tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(app);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await expectLater(
+          find.byType(NavBar), matchesGoldenFile('goldens/dining_active.png'));
+    });
+
+    testGoldens("favorites_active", (WidgetTester tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(app);
+      await tester.tap(find.byIcon(Constants.kNavBarFavoritesIcon));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await expectLater(find.byType(NavBar),
+          matchesGoldenFile('goldens/favorites_active.png'));
+    });
+
+    testGoldens("profile_active", (WidgetTester tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(app);
+      await tester.tap(find.byIcon(Constants.kNavBarProfileIcon));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await expectLater(
+          find.byType(NavBar), matchesGoldenFile('goldens/profile_active.png'));
+    });
+  });
+}
