b2cadabe66664937559af4b5894c39bf2ea63370
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-10-23 17:43:24 -0400

    mongodb

--- app_backend/server.py
+++ app_backend/server.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
+
+class Datbase():
+    def __init__(self, username: str, password: str):
 
 @app.get("/")
 async def root():
