3d3c5d5b74d7565e17b659140897efd1acd27a3d
zjdavid <zjdavid.2003@gmail.com>
2021-10-27 15:21:18 -0400

    Modify friends message passing and rewrite tests

--- app_backend/app/routers/friends.py
+++ app_backend/app/routers/friends.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter, Response, status
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/friends")
+
+
+class AddFriend(BaseModel):
+    uuid: str
+    fuuid: str
+
 
 """
     add_friend implements the /new-friend route
@@ -21,25 +28,25 @@ router = APIRouter(prefix="/friends")
             The status code for the request
 """
 @router.post("/new-friend/", status_code=status.HTTP_201_CREATED)
-async def add_friend(resp: Response, uuid: str = None, fuuid: str = None):
+async def add_friend(resp: Response, friend: AddFriend):
     # check if the uuid exists
-    if uuid is None or not isinstance(uuid, str) or uuid == "":
+    if friend.uuid == "":
         resp.status_code = status.HTTP_400_BAD_REQUEST
         return {"err": "Invalid UUID"}
-    if fuuid is None or not isinstance(fuuid, str) or fuuid == "":
+    if friend.fuuid == "":
         resp.status_code = status.HTTP_400_BAD_REQUEST
         return {"err": "Invalid Friend UUID"}
 
     # check if the uuid does not already contain fuuid
-    if uuid == fuuid:
+    if friend.uuid == friend.fuuid:
         resp.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return {"err" : "Invalid combination of UUIDs"}
 
     uuid_friends = ["test_uuid"]  # TODO: get uuid's friends here
-    if fuuid in uuid_friends:
+    if friend.fuuid in uuid_friends:
         resp.status_code = status.HTTP_412_PRECONDITION_FAILED
         return {"err" : "FUUID already linked to UUID"}
 
     # add fuuid
-    uuid_friends.append(fuuid)
+    uuid_friends.append(friend.fuuid)
     return {"msg" : "FUUID has been successfully linked to UUID"}
