f779b9ca5a740339092273a5bfea028df1bcde4f
mimiolayeye <mimi.olayeye@emory.edu>
2021-11-01 11:52:01 -0400

    Save seated section (app backend) (updated)

--- app_backend/balance.py
+++ app_backend/balance.py
@@ -2,8 +2,8 @@ from user import User
 
 
 class BalanceTracker(User):
-    def __init__(self, name: str, dooley_dollars: float, meal_swipes: int):
-        super().__init__(name, dooley_dollars, meal_swipes)
+    def __init__(self, UUID: str, dooley_dollars: float, meal_swipes: int):
+        super().__init__(UUID, dooley_dollars, meal_swipes)
         """
 
         name: str
@@ -22,39 +22,50 @@ class BalanceTracker(User):
     def dooley_sub(self, spent: float):
         """
         spent: The amount of dooley dollars spent
-        return: The updated Balance Tracker object
+        return: True or False (whether operation succeeds or fails)
         """
-        if (abs(spent) <= self.dooley_dollars):
-            self.dooley_dollars = self.dooley_dollars - abs(spent)
-        elif (abs(spent) > self.dooley_dollars): # else if
-            self.dooley_dollars = 0
-        return self
+        if (spent < 0 ): # Error (negative value)
+            return False
+        if (spent <= self.dooley_dollars): # No error (possible value)
+            self.dooley_dollars = self.dooley_dollars - spent
+            return True
+        elif (spent > self.dooley_dollars): # Error (impossible value)
+            return False
+
 
     def mealswipe_sub(self):
         """
         Takes no params -- decrements when called
-        return: The updated Balance Tracker object
+        return: True or False (whether operation succeeds or fails)
         """
         if (self.meal_swipes > 0):
             self.meal_swipes = self.meal_swipes - 1
-        return self
+            return True
+        if (self.meal_swipes <= 0): # Should never be negative
+            return False
 
     # ------------ Incrementing Balances (for deposit) -------------
 
-    def dooley_add(self, additional):
+    def dooley_add(self, additional: float):
 
         """
         additional: The amount of extra dooley dollars added
-        return: The updated Balance Tracker object
+        return: True or False (whether operation succeeds or fails)
         """
-        self.dooley_dollars = self.dooley_dollars + abs(additional)
-        return self
+        if (additional <= 0):
+            return False
+        elif(additional > 0):
+            self.dooley_dollars = self.dooley_dollars + additional
+            return True
 
-    def mealswipe_add(self, additional):
+    def mealswipe_add(self, additional: int):
         """
         additional: The amount of extra swipes added
-        return: The updated Balance Tracker object
+        return: True or False (whether operation succeeds or fails)
         """
-        self.meal_swipes = self.meal_swipes + abs(additional)
-        return self
+        if (additional <= 0 ):
+            return False
+        elif (additional > 0 ):
+            self.meal_swipes = self.meal_swipes + additional
+            return True
 
--- app_backend/balance_test.py
+++ app_backend/balance_test.py
@@ -22,54 +22,76 @@ def test_update_balance():
     ]
 
     dooley_deposit = [ # test deposit
-        500, 60, 450
+        500, 60, 450, 44.34, -50, -235
     ]
 
     dooley_spending = [
-        3, 13.99, 2.50, 5.55, 300.25, 74.39, 4000, 1.99 #test spending
+        3, 13.99, 2.50, 5.55, 300.25, 74.39, 4000, 1.99, -78.5 #test spending
     ]
 
     swipe_deposit = [ # test deposit
-        30, 25, 10
+        3.3, 30, 25, 10, -40, 0, 1, -3.4
     ]
     # -------- test 1: dooley spending --------
     for test_case in test_cases:
-        Tester = BalanceTracker(test_case[0], test_case[1], test_case[2])
+        tester = BalanceTracker(test_case[0], test_case[1], test_case[2])
         dooley_dollars_current = test_case[1]
 
         for dooleys_spent in dooley_spending:
-            Tester.dooley_sub(dooleys_spent)
-            if( abs(dooleys_spent) <= Tester.dooley_dollars):
-                assert Tester.dooley_dollars == (dooley_dollars_current - abs(dooleys_spent))
-                assert Tester.name == test_case[0]
-                assert Tester.meal_swipes == test_case[2]
-            elif (abs(dooleys_spent) >Tester.dooley_dollars):
-                assert Tester.dooley_dollars == 0
-                assert Tester.name == test_case[0]
-                assert Tester.meal_swipes == test_case[2]
+            test = tester.dooley_sub(dooleys_spent)
+            if( 0 < dooleys_spent <= tester.dooley_dollars):
 
-            dooley_dollars_current = dooley_dollars_current - dooleys_spent # updating
+                assert test == True # No Error
+                assert tester.dooley_dollars == (dooley_dollars_current - abs(dooleys_spent))
+                assert tester.name == test_case[0]
+                assert tester.meal_swipes == test_case[2]
+                dooley_dollars_current = dooley_dollars_current - dooleys_spent # updating
+
+            elif (dooleys_spent > tester.dooley_dollars or dooleys_spent < 0):
+
+                assert test == False # Error
+                assert tester.dooley_dollars == dooley_dollars_current
+                assert tester.name == test_case[0]
+                assert tester.meal_swipes == test_case[2]
 
         # -------- test 2: swipe deposit --------
     for test_case in test_cases:
-        Tester = BalanceTracker(test_case[0], test_case[1], test_case[2])
+        tester = BalanceTracker(test_case[0], test_case[1], test_case[2])
         meal_swipe_current = test_case[2]
 
         for swipes in swipe_deposit:
-            Tester.mealswipe_add(swipes)
-            assert Tester.meal_swipes == meal_swipe_current + abs(swipes)
-            assert Tester.name == test_case[0]
-            assert Tester.dooley_dollars == test_case[1]
-            meal_swipe_current = meal_swipe_current+swipes
+            test = tester.mealswipe_add(swipes)
+            if (swipes <= 0 ):
+                assert test == False
+                assert tester.meal_swipes == meal_swipe_current
+                assert tester.name == test_case[0]
+                assert tester.dooley_dollars == test_case[1]
+
+            elif (swipes > 0 ):
+                assert test == True
+                assert tester.meal_swipes == meal_swipe_current + swipes
+                assert tester.name == test_case[0]
+                assert tester.dooley_dollars == test_case[1]
+                meal_swipe_current = meal_swipe_current+swipes # Update
+            # print("SWIPES: ", tester.meal_swipes)
 
         # -------- test 3: dooley deposit --------
     for test_case in test_cases:
-        Tester = BalanceTracker(test_case[0], test_case[1], test_case[2])
+        tester = BalanceTracker(test_case[0], test_case[1], test_case[2])
         dooley_dollars_current = test_case[1]
 
         for dooleys_added in dooley_deposit:
-            Tester.dooley_add(dooleys_added)
-            assert Tester.dooley_dollars == dooley_dollars_current + abs(dooleys_added)
-            assert Tester.name == test_case[0]
-            assert Tester.meal_swipes == test_case[2]
-            dooley_dollars_current = dooley_dollars_current + dooleys_added
+            test = tester.dooley_add(dooleys_added)
+
+            if (dooleys_added <= 0 ):
+                assert test == False
+                assert tester.dooley_dollars == dooley_dollars_current
+                assert tester.name == test_case[0]
+                assert tester.meal_swipes == test_case[2]
+
+            elif (dooleys_added > 0):
+                assert test == True
+                assert tester.dooley_dollars == dooley_dollars_current + dooleys_added
+                assert tester.name == test_case[0]
+                assert tester.meal_swipes == test_case[2]
+                dooley_dollars_current = dooley_dollars_current + dooleys_added
