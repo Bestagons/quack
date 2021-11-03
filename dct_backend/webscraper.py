24ff4ecbc5b1b7e8f3098cd1d89662feb87b89a8
Miles <72813078+Foxworth22@users.noreply.github.com>
2021-11-03 15:55:23 -0400

    first commit to scraper branch

--- dct_backend/webscraper.py
+++ dct_backend/webscraper.py
@@ -7,16 +7,31 @@ req = requests.get("https://emoryatlanta.cafebonappetit.com/cafe/dobbs-common-ta
 soup = BeautifulSoup(req.content, "html.parser")
 
 res = soup.title
+print(res.prettify()) # print(res.get_text())
 
+# Scrape Mealtime Names
 mealmenu = soup.findAll('section', attrs = {'class':'panel s-wrapper site-panel site-panel--daypart'})
 mealmenu.extend(soup.findAll('section', attrs = {'class':'panel s-wrapper site-panel site-panel--daypart site-panel--daypart-even'}))
-
 for row in mealmenu:
     print(row.find('h2', class_='panel__title site-panel__daypart-panel-title').get_text())
+print('Number of meals today = ' + str(len(mealmenu)))
 
-print(len(mealmenu))
+# Scrape Meal Times
+mealtime = soup.findAll('div', attrs = {'class':'site-panel__daypart-time'})
+for row in mealtime:
+    print(row.get_text())
 
-# for row in mealmenu:
-#     print(row.get_text)
+# Scrape Food Items
+fooditem = soup.findAll('button', attrs = {'class':'h4 site-panel__daypart-item-title'})
+for row in fooditem:
+    print(row.get_text().strip())
 
-# print(res.get_text())
+# Scrape Meal Station Names
+mealstation = soup.findAll('h3', attrs = {'class':'site-panel__daypart-station-title'})
+mealstations = []
+for row in mealstation:
+    if row.get_text() not in mealstations:
+        mealstations.append(row.get_text())
+print(mealstations)
+
+# Scrape Calories
