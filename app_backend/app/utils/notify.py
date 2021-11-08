d104395bef8e0567e5a6bec953d43269d6126da3
zjdavid <zjdavid.2003@gmail.com>
2021-11-08 15:37:49 -0500

    Wrote Notify Multiple

--- app_backend/app/utils/notify.py
+++ app_backend/app/utils/notify.py
@@ -19,3 +19,23 @@ def notify_one(token: str, title: str, body: str) -> str:
         token=token
     )
     return messaging.send(message)
+
+
+def notify_multiple(tokens: list, title: str, body: str) -> str:
+    """
+
+    :param tokens: The registration token of the device to which the message should be sent
+    :param title: Notification title
+    :param body: Notification body
+    :return:
+    Returns:
+        string: A message ID string that uniquely identifies the sent message.
+    """
+    message = messaging.MulticastMessage(
+        tokens=tokens,
+        notification=messaging.Notification(
+            title=title,
+            body=body
+        )
+    )
+    return messaging.send(message)
