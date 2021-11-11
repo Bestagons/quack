5b7b163c510b3a238c1f59222e4102c1f14ee32d
zjdavid <zjdavid.2003@gmail.com>
2021-11-10 23:25:32 -0500

    Implements basic add device

--- app_backend/app/routers/notification.py
+++ app_backend/app/routers/notification.py
@@ -3,3 +3,23 @@ from pydantic import BaseModel
 
 router = APIRouter(prefix="/notification")
 
+"""
+    add_device binds a device to a user
+
+    resp: Response
+        The response to send back to the user which contains the status code and body
+
+    device_code: str
+        The UUID of the friend to be added
+
+    returns Response
+        Response.body: dict
+            Provides any msgs/errs for the request
+        Response.status: int
+            The status code for the request
+"""
+@router.post("/add_device")
+async def add_device(resp: Response, device_code: str):
+    resp.status = status.HTTP_200_OK
+    resp.body = {"msg": "Device added successfully"}
+    return resp
