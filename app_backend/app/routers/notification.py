385e7ec3eb11c8d6ec1a3a307b8a280b47adf9a2
zjdavid <zjdavid.2003@gmail.com>
2021-11-15 14:28:52 -0500

    Move User model, add logic for adding & removing devices

--- app_backend/app/routers/notification.py
+++ app_backend/app/routers/notification.py
@@ -28,7 +28,52 @@ db.connect()
 """
 @router.post("/add_device")
 async def add_device(resp: Response, device_code: str):
-    user = User()
+    user = User()  # Todo: Get User from JWT
+    # Add the device to the user
+    devices = user.devices
+    if devices is None:
+        devices = [device_code]
+    elif device_code in devices:  # Allows multiple add.
+        resp.body = {"msg": "Device already added"}
+        return resp
+    else:
+        devices.append(device_code)
+    # Update the user
+    db.db["users"].update_one({"_id": user.id}, {"$set": {"devices": devices}})
+
     resp.status = status.HTTP_200_OK
     resp.body = {"msg": "Device added successfully"}
+    return resp
+
+
+"""
+    add_device binds a device to a user
+
+    resp: Response
+        The response to send back to the user which contains the status code and body
+
+    device_code: str
+        The UUID of the friend to be added
+
+    returns Response
+        Response.body: dict
+            Provides any msgs/errs for the request
+        Response.status: int
+            The status code for the request
+"""
+@router.post("/remove_device")
+async def add_device(resp: Response, device_code: str):
+    user = User()  # Todo: Get User from JWT
+    # Add the device to the user
+    devices = user.devices
+    if devices is None or device_code not in devices:
+        resp.body = {"msg": "Device not found"}
+        return resp
+    else:
+        devices.remove(device_code)
+    # Update the user
+    db.db["users"].update_one({"_id": user.id}, {"$set": {"devices": devices}})
+
+    resp.status = status.HTTP_200_OK
+    resp.body = {"msg": "Device removed successfully"}
     return resp
--- app_backend/models/user.py
+++ app_backend/models/user.py
@@ -1,4 +1,20 @@
-from pydantic import BaseModel
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
 
 """
     UserLogin model keeps track of the user login information
@@ -10,6 +26,7 @@ from pydantic import BaseModel
         The password of the user
 """
 class User(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str
     email: str
     password: str
