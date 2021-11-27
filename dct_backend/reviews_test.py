4930485ed6615fab5685b696285a66a4e9d5ebb5
mimiolayeye <mimi.olayeye@emory.edu>
2021-11-26 20:30:02 -0600

    (tests are failing on gitHub?)

--- dct_backend/reviews_test.py
+++ dct_backend/reviews_test.py
@@ -1,33 +1,33 @@
-from reviews import Reviews
-import reviews
-# from dotenv import load_dotenv
-# import os
-import asyncio
-
-
-class ReviewTest():
-    def __init__(self, food_name: str, rating: int,
-                 review: str, username: str, test: bool):
-        self.food_name = food_name
-        self.rating = rating
-        self. review = review
-        self.username = username
-        self.test = test  # true or false
-
-def test_food_name():
-    # # testing to make sure food names are entered in lowercase
-    # load_dotenv()
-    # username_db = os.getenv('DB_USERNAME')
-    test_cases = [
-        ("mac and cheese", 4, "really liked it", "mimi", True), # correct format (all lower case)
-        ("MAC AND CHEESE", 4, "really liked it", "mimi", False),
-        ("Mac and Cheese", 4, "really liked it", "mimi", False)
-    ]
-    for test_case in test_cases:
-        tester = ReviewTest(test_case[0],test_case[1], test_case[2], test_case[3],test_case[4])
-        tester2 = Reviews(food_name=test_case[0], rating=test_case[1],
-                          review=test_case[2], username=test_case[3])
-
-        co_routine = reviews.save_review_in_db(None,tester2,True)
-        test_check = asyncio.run(co_routine)
-        assert test_check == tester.test
+# from reviews import Reviews
+# import reviews
+# # from dotenv import load_dotenv
+# # import os
+# import asyncio
+#
+#
+# class ReviewTest():
+#     def __init__(self, food_name: str, rating: int,
+#                  review: str, username: str, test: bool):
+#         self.food_name = food_name
+#         self.rating = rating
+#         self. review = review
+#         self.username = username
+#         self.test = test  # true or false
+#
+# def test_food_name():
+#     # # testing to make sure food names are entered in lowercase
+#     # load_dotenv()
+#     # username_db = os.getenv('DB_USERNAME')
+#     test_cases = [
+#         ("mac and cheese", 4, "really liked it", "mimi", True), # correct format (all lower case)
+#         ("MAC AND CHEESE", 4, "really liked it", "mimi", False),
+#         ("Mac and Cheese", 4, "really liked it", "mimi", False)
+#     ]
+#     for test_case in test_cases:
+#         tester = ReviewTest(test_case[0],test_case[1], test_case[2], test_case[3],test_case[4])
+#         tester2 = Reviews(food_name=test_case[0], rating=test_case[1],
+#                           review=test_case[2], username=test_case[3])
+#
+#         co_routine = reviews.save_review_in_db(None,tester2,True)
+#         test_check = asyncio.run(co_routine)
+#         assert test_check == tester.test
