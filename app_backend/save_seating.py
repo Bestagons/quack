4468addb8b85fd86d1ae957617ba9a679fad13d6
mimiolayeye <mimi.olayeye@emory.edu>
2021-11-26 20:05:22 -0600

    seating section

--- app_backend/save_seating.py
+++ app_backend/save_seating.py
@@ -1,32 +1,61 @@
 from sections import Sections
 
-"""
-Section: The seating section (numbered from 1 to 9)
-Color: The respective color associated with each seating section
+from fastapi import APIRouter, Response, status
+from dotenv import load_dotenv
+import os
+from database import Database
+from pydantic import BaseModel
+import server
 
-"""
+load_dotenv()
+db = Database(os.getenv('DB_USERNAME'), os.getenv('DB_PASSWORD'))
+db.connect()
 
-class SaveSeating():
 
-    def __init__(self):
-        self.section = None
-        self.color = None
+router = APIRouter(prefix="/seating")
+resp = Response
 
-    def set_section_info(self, section:int) -> bool:
-        """
 
-        This function updates the object with the section number and respective color
 
-        section: The seating section number (based off of @BrendaCano 's Map)
-        return: True or False (error handling)
+class SaveSeating(BaseModel):
+    """
+        section: The seating section (numbered from 1 to 9)
 
-        """
-        if 0 < section <= 9:
-            self.section = section
-            self.color = Sections.color_dict.get(self.section)
+        color: The respective color associated with each seating section
+
+    """
+    section: int
+    color: str
+
+
+@router.post("/dct-seating-section", status_code=status.HTTP_201_CREATED)
+async def set_section_info(resp: Response, seating_info: SaveSeating, dry_run) -> bool:
+    """
+
+    This function updates the object with the section number and respective color
+
+    section: The seating section number (based off of @BrendaCano 's Map)
+    return: True or False (error handling)
+
+    """
+    if dry_run:
+        if 0 < seating_info.section <= 9:
+
             return True
         else:
             return False
+
+
+    if (not dry_run):
+        if seating_info.section == "":
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+            return {"err": "No seating section number specified"}
+        if seating_info.color == "":
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+            return {"err": "No seating section color specified"}
+
+
+
 
 
 
