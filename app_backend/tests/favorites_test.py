4238396b1705bfd3111947945cee007af5dd70d8
zjdavid <zjdavid.2003@gmail.com>
2021-11-29 14:05:46 -0500

    Update tests

--- app_backend/tests/favorites_test.py
+++ app_backend/tests/favorites_test.py
@@ -1,11 +1,14 @@
 from app.app import app
 from fastapi.testclient import TestClient
 from fastapi import status
+from .authenticate_test import authenticate_test
 
 client = TestClient(app)
 
 
 def test_toggle_favorite():
+    token = authenticate_test()
+
     class Params:
         def __init__(self, food_name: str, state: bool, expected_status_code: int, expects_err: bool):
             self.food_name = food_name
@@ -20,6 +23,7 @@ def test_toggle_favorite():
     for test_case in test_cases:
         response = client.post(
             "/favorites/toggle-favorite/",
+            headers={"Authorization": f"Bearer {token}", "Accept": "application/json"},
             json={"food_name": test_case.food_name, "state": test_case.state},
         )
         print("Testing: " + response.url + " with food_id: " + test_case.food_name +
