d83625485fd25c889206a675d879f99704ae1f29
mimiolayeye <mimi.olayeye@emory.edu>
2021-11-29 17:27:30 -0600

    (update: fixed test)

--- app_backend/tests/seat_test.py
+++ app_backend/tests/seat_test.py
@@ -1,5 +1,5 @@
-from app_backend.save_seating import SaveSeating
-from app_backend.sections import Sections
+from ..save_seating import SaveSeating
+from ..sections import Sections
 
 def test_set_seat():
     section = []
