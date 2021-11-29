8c4041147ddb4b085fd78fff4854147df1bf5811
mimiolayeye <mimi.olayeye@emory.edu>
2021-11-29 10:02:27 -0600

    (update and response)

--- dct_backend/reviews_test.py
+++ dct_backend/reviews_test.py
@@ -1,4 +1,5 @@
 from reviews import Reviews
+from fastapi import APIRouter, Response, status
 import reviews
 import asyncio
 
@@ -24,6 +25,6 @@ def test_food_name():
         tester2 = Reviews(food_name=test_case[0], rating=test_case[1],
                           review=test_case[2], username=test_case[3])
 
-        co_routine = reviews.save_review_in_db(None,tester2,True)
+        co_routine = reviews.save_review_in_db(Response,tester2,True)
         test_check = asyncio.run(co_routine)
         assert test_check == tester.test
