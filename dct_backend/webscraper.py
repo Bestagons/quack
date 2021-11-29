fbe59aa7d02d205b19a156e3a028ee4e82c4746d
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-11-29 13:40:25 -0500

    integrated scraper with dct_backend

--- dct_backend/webscraper.py
+++ dct_backend/webscraper.py
@@ -1,19 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import dct
-
-# DCT import
-# DCT = dct.DCT()
 
 def createSoup(url):
     # setup BeautifulSoup with URL
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
-
-    # Title of webpage
-    title = soup.title
-    # print(title.prettify()) #prettify() can be a useful beautifulsoup method
-    title = title.get_text()
     return soup
 
 def scrapeFood(mealtime, mealname, station_soup):
@@ -33,25 +24,7 @@ def scrapeFood(mealtime, mealname, station_soup):
             foodcal = int(item_soup.find('div', attrs = {'class':'site-panel__daypart-item-calories'}).get_text().replace('cal.','').strip())
         else:
             foodcal = -1
-        # # printing
-        # print('foodname: ' + str(foodname))
-        # if foodcats:
-        #     for foodcat in foodcats:
-        #             print(foodcat)
-        # else:
-        #     foodcats = []
-        # print(' foodcal: ' + str(foodcal))
-        # print('mealname: ' + str(mealname))
-        # print('    time: ' + str(mealtime))
-        # print(' station: ' + str(stationName))
-        # print("")
 
-        # # saving
-        # DCT.save_food_item(foodname, stationName, mealtime, foodcal, foodcats)
-        # TODO: save mealname?
-
-        # return statement for logging purposes
-        # return "At " + str(stationName) + " from " + str(mealtime) + ", " + str(foodname) + " is served."  
         return foodname, stationName, mealtime, foodcal, foodcats # this is an immutable touple
 
 def scrapeFrame(soup):
@@ -66,54 +39,4 @@ def scrapeFrame(soup):
             log.append(scrapeFood(mealtime, mealname, station_soup))
     return log
 
-# MAIN
-soup = createSoup("https://emoryatlanta.cafebonappetit.com/cafe/dobbs-common-table/")
-output = scrapeFrame(soup)
 
-# printing
-print(len(output))
-# print(output[0])
-# print(DCT.menu)
-# print('food served today:')
-# for item in DCT.menu:
-#     print(item.name)
-# print('Stations:')
-# print(DCT.stations)
-
-# Test single scrape
-# scrapeFood('testTime', 'testName', soup)
-
-# Test DCT
-# print(DCT.save_food_item('oreos', 'CRC','12:00am', 1000, ['vegan']))
-# print(DCT.menu)
-# for item in DCT.menu:
-#     print(item.name)
-# print('Stations:')
-# print(DCT.stations)
-
-#----------------- Useful first scrapes ---------------------------
-
-# Scrape Mealtime Names from Frames
-# mealmenu = soup.findAll('section', attrs = {'class':'panel s-wrapper site-panel site-panel--daypart'})
-# mealmenu.extend(soup.findAll('section', attrs = {'class':'panel s-wrapper site-panel site-panel--daypart site-panel--daypart-even'}))
-# # for row in mealmenu:
-# #     print(row.find('h2', class_='panel__title site-panel__daypart-panel-title').get_text())
-# # print('Number of meals today = ' + str(len(mealmenu)))
-
-# # Scrape Meal Times
-# mealtime = soup.findAll('div', attrs = {'class':'site-panel__daypart-time'})
-# # for row in mealtime:
-# #     print(row.get_text())
-
-# # Scrape Food Items
-# fooditem = soup.findAll('button', attrs = {'class':'h4 site-panel__daypart-item-title'})
-# # for row in fooditem:
-# #     print(row.get_text().strip())
-
-# # Scrape Meal Station Names
-# mealstation = soup.findAll('h3', attrs = {'class':'site-panel__daypart-station-title'})
-# mealstations = []
-# for row in mealstation:
-#     if row.get_text() not in mealstations:
-#         mealstations.append(row.get_text())
-# print(mealstations)
