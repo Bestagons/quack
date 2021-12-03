b68fe73e3642173704616a53b2cb48857c5d65fa
oreorz <oreadenikinju@gmail.com>
2021-12-03 17:00:30 -0500

    Implemented new search-friend route to search for users in the database

--- app_backend/app/routers/friends.py
+++ app_backend/app/routers/friends.py
@@ -15,6 +15,36 @@ router = APIRouter(prefix="/friends")
 security = JWTBearer() 
 
 """
+    search_friend implements the /search-friend route
+
+    resp: Response
+        The response to send back to the user which contains the status code and body
+
+    friend: AddFriend
+        Contains the email of the user that will be searched 
+
+    returns Response
+        Response.body: dict
+            Provides any msgs/errs for the request
+        Response.status: int
+            The status code for the request
+"""
+@router.post("/search-friend", status_code=status.HTTP_201_CREATED)
+async def search_friend(resp: Response, friend: AddFriend):
+    if friend.friend_email == "":
+        resp.status_code = status.HTTP_400_BAD_REQUEST
+        return {"err": "Invalid Friend UUID"}
+
+    # check if friend email exists in database
+    friend_user = User().get_user_by_email(friend.friend_email)
+    if friend_user.name == "":
+        resp.status_code = status.HTTP_404_NOT_FOUND
+        return {"err": "User with this email does not exist"}
+
+    # return friend name
+    return friend_user.name
+
+"""
     add_friend implements the /new-friend route
 
     resp: Response
@@ -36,7 +66,7 @@ security = JWTBearer()
 async def add_friend(resp: Response, friend: AddFriend, token: HTTPAuthorizationCredentials = Security(security), dry_run: bool = False):
     payload: dict = decodeJWT(token)
     user_id = payload["user_id"]
-    user: User = User().get_user_by_id(user_id)
+    user = User().get_user_by_id(user_id)
 
     if friend.friend_email == "":
         resp.status_code = status.HTTP_400_BAD_REQUEST
@@ -62,7 +92,6 @@ async def add_friend(resp: Response, friend: AddFriend, token: HTTPAuthorization
         user.save()
     return {"msg": "FUUID has been successfully linked to UUID"}
 
-
 """
     remove_friends implements the /remove-friend route
 
@@ -82,7 +111,7 @@ async def add_friend(resp: Response, friend: AddFriend, token: HTTPAuthorization
 async def remove_friend(resp: Response, friend: AddFriend, token: HTTPAuthorizationCredentials = Security(security), dry_run: bool = False):
     payload: dict = decodeJWT(token)
     user_id = payload["user_id"]
-    user: User = User().get_user_by_id(user_id)
+    user = User().get_user_by_id(user_id)
 
     if friend.friend_email == "":
         resp.status_code = status.HTTP_400_BAD_REQUEST
